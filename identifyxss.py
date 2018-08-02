853adf112f4796946870d9b20827137cb1059c02
Praveen Sutar <psutar@qualys.com>
2018-08-02 12:12:30 +0530

    Initial Code push

---
+++ identifyxss.py
@@ -0,0 +1,35 @@
+import requests, re
+
+
+### Read payload file for xss attack testing
+fname = "payloads.txt"
+with open(fname) as f:
+    content = f.readlines()
+# Remove  `\n`
+pattern = '/^((http|https):\/\/)/'
+payloads = [x.strip() for x in content]
+url = raw_input("URL: ")
+
+test = re.findall('http[s]?://', url)
+#print(test)
+if test:
+    print("### Protocol present")
+else:
+    print('### Adding protocol in URL ')
+    url = 'http://'+url
+    print("#### Input URL=  %s" % (url))
+vuln = []
+try:
+    for payload in payloads:
+        payload = payload
+        new_url = url+'/'
+        xss_url = new_url+payload
+        r = requests.get(xss_url, verify=False)
+        if payload.lower() in r.text.lower():
+            print("Vulnerable: " + payload)
+            if(payload not in vuln):
+                vuln.append(payload)
+        else:
+            print("URL Not vulnerable!!")
+except:
+    print("#### Exceptions in URL....")
